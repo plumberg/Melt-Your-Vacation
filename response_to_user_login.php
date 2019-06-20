af12806e6653a599ad40d99ccda4c12c4203b4b6
Plumberg <hana.ryzhuk@gmail.com>
2019-06-20 09:04:51 -0400

    shortened code for php version checking if user exists

--- response_to_user_login.php
+++ response_to_user_login.php
@@ -13,26 +13,17 @@ $pass = $_POST['password'];
 require ('config/db_conn.php');
 if ($user&&$pass)
 {
-$query1 = <<<SQL
-    SELECT * FROM authorizedusers WHERE userName='$user'
-SQL;
-$resultUser=$conn->query($query1);
-$row1=$resultUser->fetch_assoc();
-$_SESSION['userID'] = $row1['user_id'];
-
-$query2  = <<<SQL
-    SELECT * FROM authorizedusers WHERE password='$pass'
-SQL;
-$resultPass=$conn->query($query2);
-$row2=$resultPass->fetch_assoc();
-
-     if($user == $row1['userName'] && $pass == $row2['password'])
-        {
-        $_SESSION['LoggedIn'] = true;
-        setcookie('cookieName', $_SESSION['name'], time() + 4800);
-        }
-
-
+	$query = <<<SQL
+	SELECT * FROM authorizedusers WHERE userName='$user' and password='$pass'
+	SQL;
+	$result=$conn->query($query);
+	$row=$result->fetch_assoc();
+	$_SESSION['userID'] = $row['user_id'];
+	if($user == $row['userName'] && $pass == $row['password'])
+	{
+		$_SESSION['LoggedIn'] = true;
+		setcookie('cookieName', $_SESSION['name'], time() + 4800);
+	}
 }
 
 /*
